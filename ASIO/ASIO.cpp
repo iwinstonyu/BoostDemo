6fd5debeb7d69520cad1e830868013b197b6b64c
windpenguin <windpenguin@gmail.com>
2017-07-07 17:29:26 +0800

    add asio timer

--- ASIO/ASIO.cpp
+++ ASIO/ASIO.cpp
@@ -4,23 +4,12 @@
 #include "stdafx.h"
 
 #include <iostream>
-#include <boost/asio.hpp>
-#include <boost/date_time/posix_time/posix_time.hpp>
+#include "AsioTimer.h"
 
-
-void AsioTimer()
-{
-	boost::asio::io_service io;
-
-	boost::asio::deadline_timer t(io, boost::posix_time::seconds(5));
-	t.wait();
-
-	std::cout << "Hello, world!" << std::endl;
-}
 
 int main()
 {
-	AsioTimer();
+	SyncTimer();
 
 	system("pause");
     return 0;
---
+++ ASIO/AsioTimer.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <iostream>
+#include <boost/asio.hpp>
+#include <boost/date_time/posix_time/posix_time.hpp>
+
+void SyncTimer()
+{
+	boost::asio::io_service io;
+
+	boost::asio::deadline_timer t(io, boost::posix_time::seconds(5));
+	t.wait();
+
+	std::cout << "Hello, world!" << std::endl;
+}
